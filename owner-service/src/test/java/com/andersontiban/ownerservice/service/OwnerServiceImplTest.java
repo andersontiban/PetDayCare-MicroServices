c2625222972ff6478262a668cada009b66dbfa2f
Anderson Tiban <andersontiban@Andersons-MacBook-Air-2.local>
2024-06-09 17:56:23 -0400

    test

--- owner-service/src/test/java/com/andersontiban/ownerservice/service/OwnerServiceImplTest.java
+++ owner-service/src/test/java/com/andersontiban/ownerservice/service/OwnerServiceImplTest.java
@@ -7,11 +7,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,14 +48,19 @@ class OwnerServiceImplTest {
 
     @Test
     void addOwner() {
-        //when
+        //given
         OwnerEntity testOwner = new OwnerEntity(
                 1L,
                 "test",
                 "9083323434");
+        //when
         ownerService.addOwner(testOwner);
         //then
-        verify(ownerRepository).save(testOwner);
+        ArgumentCaptor<OwnerEntity> ownerArgumentCaptor = ArgumentCaptor.forClass(OwnerEntity.class); // this line goes to the actual ownerRepo.save(owner) and gets the owner from the param
+        verify(ownerRepository).save(ownerArgumentCaptor.capture());
+
+        OwnerEntity capturedOwner = ownerArgumentCaptor.getValue();
+        assertThat(capturedOwner).isEqualTo(testOwner);
 
     }
 
@@ -64,4 +72,17 @@ class OwnerServiceImplTest {
         verify(ownerRepository).findById(1L);
 
     }
+
+//    @Test
+//    void updateOwner() {
+//        OwnerEntity testOwner = new OwnerEntity(
+//                1L,
+//                "test",
+//                "9083323434");
+//         //when
+//        ownerService.updateOwner(eq(1L), any(OwnerEntity.class));
+//
+//        //then
+//        verify(ownerRepository).findById(any());
+//    }
 }
